3d1dbe163310905ab4fa2a12715e736d02fb639c
mudalib03 <mudalibabdulle17@gmail.com>
2025-04-11 10:11:54 +0300

    react hook running tes

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -6,6 +6,8 @@ import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
+<h1>hello from the Header!</h1>
 
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
+
